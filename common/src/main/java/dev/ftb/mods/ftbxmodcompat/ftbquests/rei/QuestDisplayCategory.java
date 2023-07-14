99471c00b35cbb9f8e982b577e7d4f8ab42a8e63
Des Herriott <des.herriott@gmail.com>
2023-07-14 11:31:47 +0100

    fix: fix potential infinite close/reopen loop with FTBQ & REI

--- common/src/main/java/dev/ftb/mods/ftbxmodcompat/ftbquests/rei/QuestDisplayCategory.java
+++ common/src/main/java/dev/ftb/mods/ftbxmodcompat/ftbquests/rei/QuestDisplayCategory.java
@@ -63,7 +63,10 @@ public class QuestDisplayCategory implements DisplayCategory<QuestDisplay> {
         Component text = display.getWrappedQuest().quest.getTitle().copy().withStyle(ChatFormatting.UNDERLINE);
         float x = bounds.x + bounds.width / 2f;
         float y = bounds.y + 5f;
-        widgets.add(Widgets.createClickableLabel(new Point(x, y), text, c -> display.getWrappedQuest().openQuestGui()));
+        widgets.add(Widgets.createClickableLabel(new Point(x, y), text, c -> {
+            Minecraft.getInstance().setScreen(null);  // avoid an infinite close-reopen loop between FTBQ and REI
+            display.getWrappedQuest().openQuestGui();
+        }));
 
         return widgets;
     }
