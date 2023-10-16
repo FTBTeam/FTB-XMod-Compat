7768291c2a4373aab015e42b23f0ea464b7d7f7a
Des Herriott <des.herriott@gmail.com>
2023-10-16 10:40:41 +0100

    feat: re-added KubeJS stages integration
    Was commented out in initial 1.20 port but KJS is available now

--- common/src/main/java/dev/ftb/mods/ftbxmodcompat/generic/gamestages/KubeJSStageProvider.java
+++ common/src/main/java/dev/ftb/mods/ftbxmodcompat/generic/gamestages/KubeJSStageProvider.java
@@ -1,29 +1,29 @@
 package dev.ftb.mods.ftbxmodcompat.generic.gamestages;
 
 import dev.ftb.mods.ftblibrary.integration.stages.StageProvider;
+import dev.latvian.mods.kubejs.stages.Stages;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 
 public class KubeJSStageProvider implements StageProvider {
 	@Override
 	public boolean has(Player player, String stage) {
-        return false;
-//		return Stages.get(player).has(stage);
+		return Stages.get(player).has(stage);
 	}
 
 	@Override
 	public void add(ServerPlayer player, String stage) {
-//		Stages.get(player).add(stage);
+		Stages.get(player).add(stage);
 	}
 
 	@Override
 	public void remove(ServerPlayer player, String stage) {
-//		Stages.get(player).remove(stage);
+		Stages.get(player).remove(stage);
 	}
 
 	@Override
 	public void sync(ServerPlayer player) {
-//		Stages.get(player).sync();
+		Stages.get(player).sync();
 	}
 
 	@Override
