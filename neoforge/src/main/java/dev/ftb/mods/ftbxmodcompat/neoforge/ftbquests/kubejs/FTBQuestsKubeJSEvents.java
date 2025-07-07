46c28f6a20cb9ec5b7b054d74d9ca293ace64e72
Des Herriott <des.herriott@gmail.com>
2025-07-07 16:34:39 +0100

    fix: make the target id optional in FTB Quests / KJS events

--- neoforge/src/main/java/dev/ftb/mods/ftbxmodcompat/neoforge/ftbquests/kubejs/FTBQuestsKubeJSEvents.java
+++ neoforge/src/main/java/dev/ftb/mods/ftbxmodcompat/neoforge/ftbquests/kubejs/FTBQuestsKubeJSEvents.java
@@ -9,13 +9,13 @@ public interface FTBQuestsKubeJSEvents {
 	EventGroup EVENT_GROUP = EventGroup.of("FTBQuestsEvents");
 
 	TargetedEventHandler<String> CUSTOM_TASK = EVENT_GROUP.server("customTask", () -> CustomTaskKubeEvent.class)
-			.requiredTarget(EventTargetType.STRING).hasResult();
+			.supportsTarget(EventTargetType.STRING).hasResult();
 	TargetedEventHandler<String> CUSTOM_REWARD = EVENT_GROUP.server("customReward", () -> CustomRewardKubeEvent.class)
-			.requiredTarget(EventTargetType.STRING).hasResult();
+			.supportsTarget(EventTargetType.STRING).hasResult();
 	TargetedEventHandler<String> OBJECT_COMPLETED = EVENT_GROUP.server("completed", () -> QuestObjectCompletedKubeEvent.class)
-			.requiredTarget(EventTargetType.STRING);
+			.supportsTarget(EventTargetType.STRING);
 	TargetedEventHandler<String> OBJECT_STARTED = EVENT_GROUP.server("started", () -> QuestObjectStartedKubeEvent.class)
-			.requiredTarget(EventTargetType.STRING);
+			.supportsTarget(EventTargetType.STRING);
 
 	EventHandler CUSTOM_FILTER_ITEM = EVENT_GROUP.client("customFilterItem", () -> CustomFilterItemKubeEvent.class);
 }
