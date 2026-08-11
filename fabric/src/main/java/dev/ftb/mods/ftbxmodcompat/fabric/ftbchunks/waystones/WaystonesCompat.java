cc18ac190df6b086e70e47a4c6d74607d9cbd501
Des Herriott <des.herriott@gmail.com>
2026-08-11 12:34:59 +0100

    fix: waystones compat - don't assume client player exists
    Client player can still be null when waystones updated event fires

--- fabric/src/main/java/dev/ftb/mods/ftbxmodcompat/fabric/ftbchunks/waystones/WaystonesCompat.java
+++ fabric/src/main/java/dev/ftb/mods/ftbxmodcompat/fabric/ftbchunks/waystones/WaystonesCompat.java
@@ -34,8 +34,12 @@ public class WaystonesCompat {
 	private static void updateWaystone(WaystoneUpdatedEvent event) {
 		LOGGER.trace("waystone updated: {} {}", event.waystone().getWaystoneUid(), event.waystone().getVisibility());
 		Waystone w = event.waystone();
-		if (Platform.get().env() == Env.CLIENT && (!FTBXModConfig.ONLY_SHOW_KNOWN_WAYSTONES.get() || WaystonesAPI.isWaystoneActivated(ClientUtils.getClientPlayer(), w))) {
-			WaystonesCommon.updateWaystone(w.getWaystoneUid(), new WaystoneData(w.getDimension(), new WaystoneMapIcon(w.getPos(), w.getName(), w.getVisibility() == WaystoneVisibility.GLOBAL)));
+		if (Platform.get().env() == Env.CLIENT) {
+			ClientUtils.getOptionalClientPlayer().ifPresent(player -> {
+				if (!FTBXModConfig.ONLY_SHOW_KNOWN_WAYSTONES.get() || WaystonesAPI.isWaystoneActivated(player, w)) {
+					WaystonesCommon.updateWaystone(w.getWaystoneUid(), new WaystoneData(w.getDimension(), new WaystoneMapIcon(w.getPos(), w.getName(), w.getVisibility() == WaystoneVisibility.GLOBAL)));
+				}
+			});
 		}
 	}
 }
